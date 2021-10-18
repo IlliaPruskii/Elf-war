8130ba72687a02c17be0e46a608bc86b1c3ac712
IlliaPruskii <illia.pruskii@gmail.com>
2021-10-18 19:06:57 +0300

    tiles

--- src/scenes/game-level.js
+++ src/scenes/game-level.js
@@ -1,17 +1,25 @@
 import { Scene } from '../scene';
+import { SpriteSheet } from '../sprite-sheet';
 
 export class GameLevel extends Scene {
 	constructor(game) {
-		super(game);
-
+		super(game)
+		this.tiles = new SpriteSheet({
+			imageName: 'tiles',
+			imageWidth: 640,
+			imageHeight: 640
+		})
+		this.tree = this.tiles.getSprite(7)
+		this.tree.setXY(10, 10)
 	}
 
 	init() {
-		super.init();
+		super.init()
 	}
 
 	render(time) {
-		this.game.screen.fill('#35d035')
-		super.render(time);
+		this.game.screen.fill('#000')
+		this.game.screen.drawSprite(this.tree)
+		super.render(time)
 	}
 }
